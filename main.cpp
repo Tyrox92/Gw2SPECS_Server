1837739a1dee42d8856255093694f119dd81827c
Tyrox <xingox@hotmail.de>
2016-04-11 08:59:19 +0200

    QCoreApplication
    -Changed QApplication to QCoreApplication

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 // main.cpp
 
-#include <QApplication>
+#include <QCoreApplication>
 #include "myserver.h"
 
 QTime timeOut1;
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QCoreApplication a(argc, argv);
 
     // Make a server and starts it
     MyServer server;
